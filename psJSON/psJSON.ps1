7953574bb4b100c2a75727b7a581819c7cdc11ba
gmadro <greg.madro@gmail.com>
2017-11-04 14:55:17 -0400

    initial scripts

---
+++ psJSON/psJSON.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+  PowerCLI and JSON
+.DESCRIPTION
+  Script to show some of the interactions between PowerCLI and JSON
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  <Inputs>
+.OUTPUTS
+  <Outputs>
+.NOTES
+  Version:        1.0
+  Author:         Greg Madro
+  Creation Date:  11/04/2017
+  Purpose/Change: Initial script
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+Get-Content .\psJSON.json | ConvertFrom-Json
