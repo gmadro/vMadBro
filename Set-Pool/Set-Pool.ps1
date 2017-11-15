02953d608dc6d4b56efd59d9af716fbefec90ef3
gmadro <greg.madro@gmail.com>
2017-11-14 21:24:14 -0500

    Final?

--- Set-Pool/Set-Pool.ps1
+++ Set-Pool/Set-Pool.ps1
@@ -18,6 +18,7 @@ $poolFile = Get-Content .\Set-Pool.json | ConvertFrom-Json
 # Loop through each Resource Pool on the vCenter Server
 $pools = Get-ResourcePool
 ForEach ($pool in $pools){
+
     If($pool.Name -eq "Resources"){
 
         # Check for VMs in the default resource pool
@@ -25,7 +26,7 @@ ForEach ($pool in $pools){
         $noPoolVM = get-vm | Where {$_.resourcePool -like "*Resources*"} | Select-Object -expandProperty Name
         if ($noPoolVM){
 
-            Write-Log "WARN" $logFIle "THe following VMs are in the default resource pool:"
+            Write-Log "WARN" $logFIle "The following VMs are in the default resource pool:"
             Foreach ($npvm in $noPoolVM){
 
                 Write-Log "WARN" $logFIle $npvm
@@ -37,73 +38,81 @@ ForEach ($pool in $pools){
         Write-Log "LABEL" $logFile ("Pool: {0}" -f $pool.Name)
         $vmCount = $pool.ExtensionData.vm.count
         $shares = 0
+
+        # Validate that pool exists in JSON config
+        foreach ($pf in $poolFile.Pools){
+
+            if ($pf.Name -like $pool.Name){
+
+                Write-Log "TRUE" $logFile "Pool exists in configuration."
+                $shares = $pf.Shares
+            }
+        }
     
-        # Check for vms in pool. Bypass pool if empty
-        if ($vmCount -ne 0){
+        # Check for vms in pool and no child pools
+        if ($vmCount -ne 0 -and -not $pool.ExtensionData.resourcePool){
 
             Write-Log "INFO" $logFile ("VM Count: $vmCount")
     
-            # Validate that pool exist in JSON config
-            foreach ($pf in $poolFile.Pools){
-    
-                if ($pf.Name -like $pool.Name){
-    
-                    Write-Log "TRUE" $logFile "Pool exists in configuration."
-                    $shares = $pf.Shares
-                }
-            }
+            $totalShares = 0
     
             if ($shares -ne 0){
     
-                $totalShares = $shares * $vmCount  
+                $totalShares = [int]$shares * [int]$vmCount
             }
             else{
     
-                $shares = $poolFile.Pools | Where-Object {$_.Name -eq "Default"} | Select-Object -ExpandProperty Shares
+                $shares = $poolFile.Pools | Where {$_.Name -eq "Default"} | Select-Object -ExpandProperty Shares
                 Write-Log "WARN" $logFile "No valid configuration exists for Pool. Setting shares to default value: $shares"
-                $totalShares = $shares * $vmCount
-            }
-        
-            # Change Resource Pool share values
-            try{
-    
-                $oldShares = $pool.NumCpuShares
-                if($oldShares -eq $totalShares -and $oldShares -eq $totalShares){
-    
-                    Write-Log "INFO" $logFile "Pool shares unchanged from: $oldShares"
-                }
-                else{
-      
-                    Set-ResourcePool -ResourcePool $pool.Name -NumCpuShares $totalShares -NumMemShares $totalShares
-                    Write-Log "INFO" $logFile "Pool shares changed from: $oldShares to: $totalShares"
-                }
-            }
-            Catch{
-    
-                Write-Log "ERROR" $logFile "Unable to set pool shares"
-                Write-Log "ERROR" $logFile $_
-            }
-            
+                #calculate pool shares
+                $totalShares = [int]$shares * [int]$vmCount
+            }    
         }# end vmCount
         else{
-    
+            
+            #Check for child pools
             if($pool.ExtensionData.resourcePool){
+                $parentPool = 0
     
                 Write-Log "INFO" $logFile "Contains child pools:"
                 ForEach($nestPool in $pool.ExtensionData.resourcePool){
-                    $nestPool
                     $np = Get-ResourcePool | Select-Object -ExpandProperty ExtensionData |Select-Object Name, VM, MoRef |Where {$_.MoRef -like $nestPool}
-                    Write-Log "INFO" $logFIle $np
-                    Write-Log "INFO" $logFile $np.Vm.Count
+                    Write-Log "INFO" $logFIle $np.Name
+                    Write-Log "INFO" $logFile ("VM Count: {0}" -f $np.Vm.Count)
+                    $parentPool += [int]$np.VM.Count
                 }
+                $totalVM = $parentPool + [int]$pool.ExtensionData.vm.count
+                #calculate pool shares
+                $totalShares = $totalVM * $shares
             }
             else{
     
+                # If no VMs or children are present bypass the Pool
                 Write-Log "INFO" $logFile "No VMs in Resource Pool. Bypassing..."
             }
         }
-    }#end pool
-}#end pools
+
+        # Attmept to change the Resource Pool share values
+        try{
+            
+            $oldShares = $pool.NumCpuShares
+            if($oldShares -eq $totalShares -and $oldShares -eq $totalShares){
+
+                Write-Log "INFO" $logFile "Pool shares unchanged from: $oldShares"
+            }
+            else{
+    
+                Set-ResourcePool -ResourcePool $pool.Name -NumCpuShares $totalShares -NumMemShares $totalShares
+                Write-Log "INFO" $logFile "Pool shares changed from: $oldShares to: $totalShares"
+            }
+        }
+        Catch{
+
+            Write-Log "ERROR" $logFile "Unable to set pool shares"
+            Write-Log "ERROR" $logFile $_
+        }
+    }# end pool
+}# end pools
 
 $scriptEnd = Get-Date
 $scriptDur = ($scriptEnd - $scriptStart).TotalSeconds
