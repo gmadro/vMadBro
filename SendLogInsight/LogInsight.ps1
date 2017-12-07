b3d061a2f902b320ae5c78822970de6236581c9a
Greg Madro <greg.madro@gmail.com>
2017-12-07 14:52:21 -0500

    Add files via upload

---
+++ SendLogInsight/LogInsight.ps1
@@ -0,0 +1,67 @@
+function Send-LogInsight{
+      <#
+      .SYNOPSIS
+      Send an event to specified Log Insight server.
+      
+      .DESCRIPTION
+      Send an event to log insight and get a return on failure or success
+      
+      .PARAMETER eventType
+      Event type: INFO, WARNING, ERROR
+      
+      .PARAMETER eventMsg
+      Event body
+      
+      .PARAMETER LIserver
+      Log Insight server to send event entry to
+      
+      .EXAMPLE
+      Send-LogInsight -eventType "INFO" -eventMsg "This is an event" -LIserver loginsight.vmadbro.com
+      
+      .NOTES
+      N/A
+      #>
+
+      param(
+            $eventType,
+            $eventMsg,
+            $LIserver
+      )
+
+      $agentID = $env:COMPUTERNAME
+
+      $restBody = @{
+            events = ([Object[]]([ordered]@{
+                  text = $eventMsg
+                  fields = @(
+                        [ordered]@{
+                              name = 'eventType'
+                              content = $eventType
+                        },
+                        [ordered]@{
+                              name = 'agentID'
+                              content = $agentID
+                        }
+                  )
+            }))
+      } | convertto-json -Depth 4
+
+      $restUrl = ("http://" + $LIserver + ":9000/api/v1/events/ingest/" + $agentID)
+      Write-Output ("Posting results to Log Insight server: " + $LIserver)
+
+      try {
+            $response = Invoke-RestMethod $restUrl -Method Post -Body $restBody -ContentType 'application/json' -ErrorAction stop
+            Write-Output 'REST Call to Log Insight server successful'
+            Write-Output $response
+      }
+      catch {
+            Write-Error 'REST Call failed to Log Insight server'
+            Write-Warning $_
+      }
+}
+
+$LIserver = "pcstlog2.fsillab.ford.com"
+$eventType = "INFO"
+$eventMsg = "Event message"
+
+Send-LogInsight -eventType $eventType -eventMsg $eventMsg -LIserver $LIserver
---
+++ SendLogInsight/LogInsight.py
@@ -0,0 +1,49 @@
+import requests
+import socket
+
+def send_to_log_insight(li_server, event_type, event_msg):
+    """
+    Send an event to specified Log Insight server
+
+    Arguments:
+    li_server -- Log Insight server to send event entry to
+    event_type -- Event type: INFO, WARNING, ERROR
+    event_msg -- Event body
+
+    Example:
+    send_to_log_insight('loginsight.vmadbro.com', 'INFO', 'This is an event')
+
+    Notes:
+    N/A
+    """
+
+    id_agent = socket.gethostname()
+
+    rest_url = 'http://' + li_server +':9000/api/v1/events/ingest/' + id_agent
+
+    rest_body = {"events": [{
+        "fields": [
+            {"name":"eventType", "content":event_type},
+            {"name":"agentID", "content":id_agent},
+            {"name":"eventMsg", "content":event_msg}
+            ],
+        "text": "Python Test"
+        }]
+                }
+
+    response = requests.post(rest_url, json=rest_body)
+
+    print('Posting results to Log Insight server: ' + li_server)
+
+    if response.status_code != 200:
+        print('REST Call failed to Log Insight server')
+        print(response.text)
+    else:
+        print('REST Call to Log Insight server successful')
+        print(response.text)
+
+LI_SERVER = 'pcstlog2.fsillab.ford.com'
+EVENT_TYPE = 'INFO'
+EVENT_MSG = 'This is a test'
+
+send_to_log_insight(LI_SERVER, EVENT_TYPE, EVENT_MSG)
