dad5b6e7e19b8d45c641d5e2ee95512ed5f2ce15
Greg Madro <greg.madro@gmail.com>
2019-01-28 23:23:49 -0500

    Create dockerhost.tf

---
+++ terraform/dockerhost.tf
@@ -0,0 +1,46 @@
+provider "vsphere" {
+  user           = "administrator@mvm.local"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "mvmvcsa01"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "MVMASTER"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "MVMASTERSAN"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "cluster1/Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
