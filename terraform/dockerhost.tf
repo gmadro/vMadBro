11e551504b8bac8f49433f6005b6f7306319d244
Greg Madro <greg.madro@gmail.com>
2019-02-01 00:40:38 -0500

    update disk for template

--- terraform/dockerhost.tf
+++ terraform/dockerhost.tf
@@ -48,6 +48,8 @@ resource "vsphere_virtual_machine" "vm" {
   disk {
     label = "disk0"
     size  = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
   }
   
   clone {
