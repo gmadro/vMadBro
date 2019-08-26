848709510d973adc2ab24060e4e4ce1fb98cf98e
Greg Madro <greg.madro@gmail.com>
2019-08-26 13:43:01 -0400

    add variables

--- serverless/create_stack.py
+++ serverless/create_stack.py
@@ -6,13 +6,16 @@ s3 = boto3.client('s3')
 
 print('Enter name of stack')
 stack = input()
-s3.upload_file('/home/lambda_test.py','vmadbro-lambda-code', 'lambda_test.py')
-temp_url = 'https://cf-templates-13om8u1v5frdu-us-east-1.s3.amazonaws.com/20192260WX-Lambda27-9utis6opq8s'
-cf.create_stack(StackName=stack, TemplateURL=temp_url)
+stack_file = stack + '.yaml'
+lambda_file = stack + '.py'
+s3.upload_file('/home/vMadBro/serverless/cf.yaml','vmadbro-cf',stack_file)
+s3.upload_file('/home/vMadBro/serverless/lambda_test.py','vmadbro-lambda-code', lambda_file)
+cf_tmpl_url = 'https://vmadbro-cf.s3.amazonaws.com/' + stack_file
+cf.create_stack(StackName=stack, TemplateURL=cf_tmpl_url)
 
-r_url = "https://api.vmadbro.com/" + stack + "/"
+app_url = "https://api.vmadbro.com/" + stack + "/"
 
-print("API created at: " + r_url)
+print("API created at: " + app_url)
 
 print('Enter first key value')
 v1 = input()
@@ -27,7 +30,7 @@ json_in = {
 test_num = 0
 
 while True:
-    r = requests.post(url = r_url, json=json_in, verify=False)
+    r = requests.post(url = app_url, json=json_in, verify=False)
     test_num = test_num + 1
     if (r.status_code == 200):
         print("API result: " + r.text)
