787952c36150f8280ea6cee1ef4fd1d469ab9538
Greg Madro <greg.madro@gmail.com>
2019-08-26 15:22:12 -0400

    Update lambda_test.py

--- serverless/lambda_test.py
+++ serverless/lambda_test.py
@@ -1,8 +1,7 @@
 import sys
-
-def main(event, context):
-    calc = event.k1 + event.k2
-    return(calc)
-
+    def main(event, context):
+        calc = event['k1'] + event['k2']
+        return(calc)
+        
 if __name__ == '__main__':
     main(event, context)
