7a6f78e4bf28badbaeb949c70843ee1fe0b3d36b
Greg Madro <greg.madro@gmail.com>
2019-08-30 08:54:17 -0400

    Add deployment get

--- serverless/create_deployment.py
+++ serverless/create_deployment.py
@@ -1,3 +1,8 @@
 import requests
 
-r = request.get
+gcp_project = 'MVM01'
+
+app_url = 'https://www.googleapis.com/deploymentmanager/v2beta1/projects/' + gcp_project + '/global/deployments'
+
+r = request.get(url = app_url, verify=False)
+print(r.text)
