a10dd668a6fa7cd30123feba5e1e5b36ce54742a
Greg Madro <greg.madro@gmail.com>
2019-08-30 12:17:52 -0400

    create request core

--- serverless/create_resource.py
+++ serverless/create_resource.py
@@ -4,6 +4,23 @@ subId = ''
 rgName = ''
 depName = ''
 
+tmplUri = ''
+paramUri = ''
+
+json_in = {
+  "properties":{
+    "templateLink": {
+      "uri": tmplUri,
+      "contentVersion": "1.0.0.0"
+    },
+    "parametersLink": {
+      "uri":paramUri,
+      "contentVersion":"1.0.0.0"
+    },
+    "mode":"Complete"
+  }
+}
+
 app_url = 'https://management.azure.com/subscriptions/' + subId + '/resourcegroups/' + rgName + '/providers/Microsoft.Resources/deployments/' + depName + '?api-version=2019-05-01'
 
 r = requests.put(url = app_url, json = json_in verify=False)
