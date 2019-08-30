5deab4c2bb262bb84a77d1b9096684d26d5d35f8
Greg Madro <greg.madro@gmail.com>
2019-08-30 09:45:43 -0400

    Update create_deployment.py

--- serverless/create_deployment.py
+++ serverless/create_deployment.py
@@ -1,8 +1,23 @@
 import requests
 
+json_in = {
+        "name":"GCPtest01",
+        "target": {
+          "config": {
+            "content" : "imports:\n- path: test_tmpl\n\nresources:\n- name: Test01\n type: test_tmpl "
+          },
+          "imports: [
+            {
+              "content":"resources:n- name: test deploy\n type: compute.v1.instance\n properties:\n zone: us-central1-a\n machineType: "
+              "name":"test_tmpl"
+            }
+          ]
+        }
+}
+
 gcp_project = 'MVM01'
 
-app_url = 'https://www.googleapis.com/deploymentmanager/v2beta1/projects/' + gcp_project + '/global/deployments'
+app_url = 'https://www.googleapis.com/deploymentmanager/v2/projects/' + gcp_project + '/global/deployments'
 
-r = requests.get(url = app_url, verify=False)
+r = requests.post(url = app_url, json = json_in verify=False)
 print(r.text)
