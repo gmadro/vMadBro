715247a26be98860b6b4f5b549f8be9735e9c452
Greg Madro <greg.madro@gmail.com>
2019-08-30 08:57:58 -0400

    syntax update

--- serverless/create_deployment.py
+++ serverless/create_deployment.py
@@ -4,5 +4,5 @@ gcp_project = 'MVM01'
 
 app_url = 'https://www.googleapis.com/deploymentmanager/v2beta1/projects/' + gcp_project + '/global/deployments'
 
-r = request.get(url = app_url, verify=False)
+r = requests.get(url = app_url, verify=False)
 print(r.text)
