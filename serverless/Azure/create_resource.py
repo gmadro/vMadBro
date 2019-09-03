c687d49fff9833bfa2644c9fdf8967b0fa644c6a
Greg Madro <admingmadro@caidan.local>
2019-09-03 16:27:11 -0400

    update azure base

--- serverless/Azure/create_resource.py
+++ serverless/Azure/create_resource.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 subId = ''
 rgName = ''
@@ -7,21 +8,10 @@ depName = ''
 tmplUri = ''
 paramUri = ''
 
-json_in = {
-  "properties":{
-    "templateLink": {
-      "uri": tmplUri,
-      "contentVersion": "1.0.0.0"
-    },
-    "parametersLink": {
-      "uri":paramUri,
-      "contentVersion":"1.0.0.0"
-    },
-    "mode":"Complete"
-  }
-}
+with open('app_resource.json') as json_file:
+  json_in = json.load(json_file)
 
 app_url = 'https://management.azure.com/subscriptions/' + subId + '/resourcegroups/' + rgName + '/providers/Microsoft.Resources/deployments/' + depName + '?api-version=2019-05-01'
 
-r = requests.put(url = app_url, json = json_in verify=False)
+r = requests.put(url = app_url, json = json_in, verify=False)
 print(r.text)
