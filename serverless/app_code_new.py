0957eec83f686a9bcf4ab3ba0b45c7b42724ed59
Greg Madro <admingmadro@caidan.local>
2019-09-10 12:16:19 -0400

    update app code

--- serverless/app_code_new.py
+++ serverless/app_code_new.py
@@ -1,6 +1,6 @@
 import sys
 def main(event, context):
-    calc = event['k1'] + event['k2'] + ' new Code'
+    calc = event['k1'] + event['k2'] + ' new Code v2'
     return(calc)
 
 if __name__ == '__main__':
