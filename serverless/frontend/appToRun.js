d1d9b4e8df64b0bcbf6e80da31b8c871a75c433f
Greg Madro <admingmadro@caidan.local>
2019-09-20 11:29:34 -0400

    testing

--- serverless/frontend/appToRun.js
+++ serverless/frontend/appToRun.js
@@ -2,4 +2,5 @@ function getFileParam() {
     var params = new URLSearchParams(document.location.search);
     var file = params.get("file");
     document.getElementById("file").innerHTML = file;
+    alert(file);
 }
