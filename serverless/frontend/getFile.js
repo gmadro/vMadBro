0cf90d6ed70fbf3f847b4085b2ece9b4e51b9888
Greg Madro <admingmadro@caidan.local>
2019-09-20 13:33:03 -0400

    update output

--- serverless/frontend/getFile.js
+++ serverless/frontend/getFile.js
@@ -1,4 +1,4 @@
 function getFileParam() {
-    var params = location.search.substring(1).split("&")[0];
-    document.getElementById("file").innerHTML = params;
+    var params = location.search.substring(1).split("&")[0].split("=")[1];
+    document.getElementById("file").innerHTML = "Run the following code: ${params}";
 }
