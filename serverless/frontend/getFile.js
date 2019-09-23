585ba5df0cead79f8fd7fe76e1790e556127d186
Greg Madro <admingmadro@caidan.local>
2019-09-23 17:51:01 -0400

    add form function

--- serverless/frontend/getFile.js
+++ serverless/frontend/getFile.js
@@ -2,3 +2,11 @@ function getFileParam() {
     var params = location.search.substring(1).split("&")[0].split("=")[1];
     document.getElementById("file").innerHTML = `Run: ${params} with:`;
 }
+function convertForm() {
+    const formData = new FormData();
+    let jsonObject = {};
+
+    for (const [key, value] of formData.entries()) {
+        jsonObject[key] = value;
+    }
+}
