958509ab40e8fadfb0cdec0ea6896389cb57f75c
Greg Madro <greg.madro@gmail.com>
2019-10-01 12:34:07 -0400

    Update getFile.js

--- serverless/frontend/getFile.js
+++ serverless/frontend/getFile.js
@@ -1,7 +1,6 @@
 function getFileParam() {
     var params = location.search.substring(1).split("&")[0].split("=")[1];
-    document.getElementById("file").innerHTML = `${params}`;
-    document.getElementById("fileLabel").innerHTML = `Run: ${params} with:`;
+    document.getElementById("file").innerHTML = params;
 }
 function convertForm() {
     var formData = JSON.stringify($("#runApp").serializeArray());
