acaaa9329c2f81098b559f487bcf9f90ffde66de
Greg Madro <greg.madro@gmail.com>
2019-10-08 14:26:42 -0400

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "terraformFunction" {
   s3_bucket = "vmadbro-lambda-code"
   s3_key = "app_code_change.zip"
   function_name = "terraformFunction"
-  role = "basic-lambda-role"
+  role = "arn:aws:iam::501511055678:role/basic-lambda-role"
   handler = "index.main"
   runtime = "python3.7"
 }
