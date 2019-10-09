65ec8b9428624a6ad87893a8b78967d0321c61f5
Greg Madro <admingmadro@caidan.local>
2019-10-09 15:07:35 -0400

    test lamdba

--- terraform/runTF.py
+++ terraform/runTF.py
@@ -1,9 +1,5 @@
 import os
 
-aws_tf_plan = 'lambda.tf'
-aws_tf_var_file = 'lambda.tfvars.json'
-gcp_tf_var_file = 'cloudFunctions.tfvars.json'
-
 cwd = os.getcwd()
 cwd_count = len(cwd.split('/'))
 tf_dir_items = cwd.split('/')[0:cwd_count - 2]
@@ -12,9 +8,8 @@ tf_dir = s.join(tf_dir_items)
 
 os.system(tf_dir + '/terraform init')
 #AWS
-#os.system(tf_dir + '/terraform plan -var-file=' + aws_tf_var_file + ' ' + cwd)
+os.system(tf_dir + '/terraform plan '  + cwd)
 #os.system(tf_dir + '/terraform apply -var-file=' + aws_tf_var_file + ' ' + cwd)
 #GCP
-print(tf_dir + '/terraform plan -var-file=' + gcp_tf_var_file + ' ' + cwd)
-os.system(tf_dir + '/terraform plan -var-file=' + gcp_tf_var_file + ' ' + cwd)
+#os.system(tf_dir + '/terraform plan -var-file=' + gcp_tf_var_file + ' ' + cwd)
 #os.system(tf_dir + '/terraform apply -var-file=' + aws_tf_var_file + ' ' + cwd)
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,28 @@
+variable "s3_bucket" {
+    type = string
+}
+
+variable "s3_key" {
+    type = string
+}
+
+variable "function_name" {
+    type = string
+}
+
+variable "role" {
+    type = string
+}
+
+variable "handler" {
+    type = string
+}
+
+variable "runtime" {
+    type = string
+}
+
+
 variable "source_archive_bucket" {
     type = string
 }
