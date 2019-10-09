29a0968b03f06637d548ce9c43c556861f131cb2
Greg Madro <admingmadro@caidan.local>
2019-10-09 15:50:04 -0400

    test

--- terraform/runTF.py
+++ terraform/runTF.py
@@ -1,15 +1,17 @@
 import os
 
-cwd = os.getcwd()
-cwd_count = len(cwd.split('/'))
-tf_dir_items = cwd.split('/')[0:cwd_count - 2]
-s = '/'
-tf_dir = s.join(tf_dir_items)
+CWD = os.getcwd()
+CWD_COUNT = len(CWD.split('/'))
+TF_DIR_ITEMS = CWD.split('/')[0:CWD_COUNT - 2]
+S = '/'
+TF_DIR = S.join(TF_DIR_ITEMS)
 
-os.system(tf_dir + '/terraform init')
+os.system(TF_DIR + '/terraform init')
+
 #AWS
-os.system(tf_dir + '/terraform plan '  + cwd)
+print(TF_DIR + '/terraform plan '  + CWD + 'aws')
 #os.system(tf_dir + '/terraform apply -var-file=' + aws_tf_var_file + ' ' + cwd)
+
 #GCP
 #os.system(tf_dir + '/terraform plan -var-file=' + gcp_tf_var_file + ' ' + cwd)
 #os.system(tf_dir + '/terraform apply -var-file=' + aws_tf_var_file + ' ' + cwd)
