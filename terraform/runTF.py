9b8b6d8d763ca36f034e9e9608eea1a9600a7f6b
Greg Madro <admingmadro@caidan.local>
2019-10-09 15:52:40 -0400

    update test

--- terraform/runTF.py
+++ terraform/runTF.py
@@ -6,11 +6,8 @@ TF_DIR_ITEMS = CWD.split('/')[0:CWD_COUNT - 2]
 S = '/'
 TF_DIR = S.join(TF_DIR_ITEMS)
 
-os.system(TF_DIR + '/terraform init')
-
-#AWS
-print(TF_DIR + '/terraform plan '  + CWD + '/aws')
-#os.system(tf_dir + '/terraform apply -var-file=' + aws_tf_var_file + ' ' + cwd)
+os.system(TF_DIR + '/terraform init ' + CWD + '/aws')
+os.system(TF_DIR + '/terraform plan ' + CWD + '/aws')
 
 #GCP
 #os.system(tf_dir + '/terraform plan -var-file=' + gcp_tf_var_file + ' ' + cwd)
