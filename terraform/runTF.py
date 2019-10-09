108b1d4d14d024e1b13a9b4a688483c1c2159926
Greg Madro <greg.madro@gmail.com>
2019-10-09 10:37:13 -0400

    Update runTF.py

--- terraform/runTF.py
+++ terraform/runTF.py
@@ -4,6 +4,6 @@ cwd = os.getcwd()
 cwd_count = len(cwd.split('/'))
 tf_dir_items = cwd.split('/')[0:cwd_count - 2]
 s = '/'
-tf_dir = s.join(app_dir_items)
+tf_dir = s.join(tf_dir_items)
 
 print(tf_dir)
