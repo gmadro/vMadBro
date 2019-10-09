527466c7ad64a47ba0559ccb881c16a78321fc26
Greg Madro <admingmadro@caidan.local>
2019-10-09 14:58:12 -0400

    create variables file

--- terraform/cloudFunctions.tf
+++ terraform/cloudFunctions.tf
@@ -1,25 +1,6 @@
-variable "source_archive_bucket" {
-    type = string
-}
-
-variable "source_archive_object" {
-    type = string
-}
-
-variable "name" {
-    type = string
-}
-
-variable "entry_point" {
-    type = string
-}
-
-variable "gcp_runtime" {
-    type = string
-}
 
 provider "google" {
-    credentials = "${file("$HOME/.gcp/credentials.json")}"
+    credentials = "${file("~/.gcp/credentials.json")}"
     project = "MVM01"
     region = "us-east1"
 }
